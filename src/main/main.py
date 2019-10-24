c236ab8c5282f104347ef30abceceeb21dc91e58
tong <1765258809@qq.com>
2019-10-24 10:51:15 +0800

    color_trans

--- src/main/main.py
+++ src/main/main.py
@@ -5,17 +5,19 @@ import cv2
 from src.main.util import ImgProcess
 from src.main.util import ann as A
 from src.main.util.CST import URL
-
+import time
 def main():
-
+    t1 = time.time()
     url=URL.getResPath("images/test/numbers.jpg")
-    ann, test_data = A.train(A.create_ann(58), 50000, 5)
+    ann, test_data = A.train(A.create_ann(500), 50000, 5)
     #A.test(ann,test_data)
     img=cv2.imread(url, cv2.IMREAD_UNCHANGED)
     img, imgbinary, rectList=ImgProcess.imgProcess(img)
     img, resultImg,resultList=ImgProcess.imgPredicted(img, imgbinary, rectList, ann)
     cv2.imshow("re",resultImg)
     print(resultList)
+    t2 = time.time()
+    print(t2)
     cv2.waitKey()
 
 
